0224cc8f8e256a58c15ae1d35d12e7fc90ee58d3
robincpc <chienpinchen@gmail.com>
2017-11-18 02:44:25 -0800

    Styling template with bootstrap and adding images

--- main_app/views.py
+++ main_app/views.py
@@ -9,15 +9,19 @@ def index(request):
 
 
 class Treasure(object):
-    def __init__(self, name, value, material, location):
+    def __init__(self, name, value, material, location, img_url):
         self.name = name
         self.value = value
         self.material = material
         self.location = location
+        self.img_url = img_url
 
 
 treasures = [
-        Treasure("Golden Nugget" , 500.00 , "gold"   , "Curly's Creek , NM"),
-        Treasure("Fool's Gold"   , 0      , "pyrite" , "Fool's Falls  , CO"),
-        Treasure("Coffee Can"    , 20.00  , "tin"    , "Acme          , CA")
+        Treasure("Golden Nugget" , 500.00 , "Gold"   , "Curly's Creek , NM",
+                "images/treasuregram-brand-gold-nugget.png"),
+        Treasure("Fool's Gold"   , 0      , "Pyrite" , "Fool's Falls  , CO",
+                "images/treasuregram-brand-fools-gold.png"),
+        Treasure("Coffee Can"    , 25.00  , "Aluminum"    , "Acme   , CA",
+                "images/treasuregram-brand-coffee-can.png")
         ]

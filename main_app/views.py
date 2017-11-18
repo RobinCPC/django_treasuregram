2ab753c0431e1184842be2d4431fb93478656bab
robincpc <chienpinchen@gmail.com>
2017-11-18 03:21:30 -0800

    Add Models (user database)

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
+from .models import Treasure
 
 # Create your views here.
 def index(request):
+    # Here we need to get all of Treasure's objects
+    treasures = Treasure.objects.all()
     context = {
             'treasures' : treasures,
             }
     return render(request, 'index.html', context)
 
-
-class Treasure(object):
-    def __init__(self, name, value, material, location, img_url):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-        self.img_url = img_url
-
-
-treasures = [
-        Treasure("Golden Nugget" , 500.00 , "Gold"   , "Curly's Creek , NM",
-                "images/treasuregram-brand-gold-nugget.png"),
-        Treasure("Fool's Gold"   , 0      , "Pyrite" , "Fool's Falls  , CO",
-                "images/treasuregram-brand-fools-gold.png"),
-        Treasure("Coffee Can"    , 25.00  , "Aluminum"    , "Acme   , CA",
-                "images/treasuregram-brand-coffee-can.png")
-        ]

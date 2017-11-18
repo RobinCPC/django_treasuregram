a0276bd4b41f437c3dff1dad31f7ba960d840c0b
robincpc <chienpinchen@gmail.com>
2017-11-18 15:36:55 -0800

    Add simple form

---
+++ main_app/forms.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django import forms
+
+class TreasureForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100)
+    value = forms.DecimalField(label='Value', max_digits=10,
+                                decimal_places=2)
+    material = forms.CharField(label='Material', max_length=100)
+    location = forms.CharField(label='Location', max_length=100)
+    img_url = forms.CharField(label='Image_URL', max_length=255)
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Treasure
+from .forms import TreasureForm
 
 # Create your views here.
 def index(request):
     # Here we need to get all of Treasure's objects
     treasures = Treasure.objects.all()
+    form = TreasureForm()
     context = {
             'treasures' : treasures,
+            'form' : form,
             }
     return render(request, 'index.html', context)
 
@@ -14,4 +18,16 @@ def index(request):
 def detail(request, treasure_id):
     treasure = Treasure.objects.get(id=treasure_id)
     return render(request, 'detail.html', {'treasure' : treasure})
+
+
+def post_treasure(request):
+    form = TreasureForm(request.Post)
+    if form.is_valid():
+        treasure = Treasure(name = form.cleaned_data['name'],
+                            value = form.cleaned_data['value'],
+                            material = form.cleaned_data['material'],
+                            location = form.cleaned_data['location'],
+                            img_url = form.cleaned_data['img_url'])
+        treasure.save()
+    return HttpResponseRedirect('/')
 

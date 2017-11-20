e41dbc764f2561613e85da66083147e7ecf1a30c
robincpc <chienpinchen@gmail.com>
2017-11-19 17:19:56 -0800

    Add user model

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+
 from .models import Treasure
 from .forms import TreasureForm
 
@@ -23,6 +25,16 @@ def detail(request, treasure_id):
 def post_treasure(request):
     form = TreasureForm(request.POST, request.FILES)
     if form.is_valid():
-        form.save(commit= True)
+        treasure = form.save(commit = False)
+        treasure.user = request.user
+        treasure.save()
     return HttpResponseRedirect('/')
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    treasures = Treasure.objects.filter(user=user)
+    return render(request, "profile.html",
+                  {"username" : username,
+                  "treasures" : treasures})
+
 

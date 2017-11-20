2435b420151420de3ca5b029d7527a4812a53ce8
robincpc <chienpinchen@gmail.com>
2017-11-19 20:52:37 -0800

    Add authentication links

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
         url(r'^post_url/$', views.post_treasure, name='post_treasure'),
         url(r'^user/(\w+)/$', views.profile, name = 'profile'),
         url(r'^login/$', views.login_view, name = 'login'),
+        url(r'^logout/$', views.logout_view, name = 'logout'),
         url(r'^register/$', views.register, name = 'register'),
         ]
 
